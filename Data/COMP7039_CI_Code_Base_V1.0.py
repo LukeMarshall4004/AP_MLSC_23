a33813e413f34a70b3c9e8fcfffaab2f7d4face5
Seliah Portugal <seliah.portugal@mycit.ie>
2023-11-24 11:56:26 +0000

    Seliah 24/11

--- Data/COMP7039_CI_Code_Base_V1.0.py
+++ Data/COMP7039_CI_Code_Base_V1.0.py
@@ -2,10 +2,10 @@ def read_integer_between_numbers(prompt, mini, maximum):
     while True:
         try:
             users_input = int(input(prompt))
-            if maximum <= users_input >= mini:
+            if maximum >= users_input >= mini:
                 return users_input
             else:
-                print(f"Numbers from {mini} to {maximum} only.")
+                print(f"Numbers from {mini} to {maximum} only. !")
         except ValueError:
             print("Sorry! Number only please") #corrected
 
@@ -226,15 +226,16 @@ def main():
     runners_name, runners_id = runners_data()
     MENU = "1. Show the results for a race \n2. Add results for a race \n3. Show all competitors by county " \
            "\n4. Show the winner of each race \n5. Show all the race times for one competitor " \
-           "\n6. Show all competitors who have won a race \n7. Quit \n>>> "
+           "\n6. Show all competitors who have won a race \n7. Show all competitors who have not won a race \n8. Quit \n>>> " # added menu 7 and moved quit to menu 8
     input_menu = read_integer_between_numbers(MENU, 1, 7)
 
-    while input_menu != 7: #keep the menu running as long as option 7 is not selected
+    while input_menu < 8: #keep the menu running as long as option 8 is not selected
         if input_menu == 1:
             id, time_taken, venue = race_results(races_location)
             fastest_runner = winner_of_race(id, time_taken)
             display_races(id, time_taken, venue, fastest_runner)
-        elif input_menu != 2:
+            
+        if input_menu == 2: # initially this was != 2
             users_venue(races_location, runners_id)
         elif input_menu == 3:
             competitors_by_county(runners_name, runners_id)
@@ -245,6 +246,8 @@ def main():
             displaying_race_times_one_competitor(races_location, runner, id)
         elif input_menu == 6:
             displaying_runners_who_have_won_at_least_one_race(races_location, runners_name, runners_id)
+        elif input_menu == 7:
+            print("This menu option is for showing competitors who havent taken a podium position in any race.")
         print()
         input_menu = read_integer_between_numbers(MENU, 1, 7)
     updating_races_file(races_location)
